d393a26c553bf40a4b0e46813f864367835396f0
Calvin La <calvinhangla.dev@gmail.com>
2020-02-18 16:45:43 -0800

    added connect-four to github pages, added carOO exercise

---
+++ carOO/car.js
@@ -0,0 +1,56 @@
+class Vehicle {
+    
+    constructor(make, model, year) {
+
+        this.make = make;
+        this.model = model;
+        this.year = year;
+
+    }
+
+    honk() {
+        return "BEEEEEP";
+    }
+
+    toString() {
+        return `The vehicle is a ${this.make} ${this.model} from ${this.year}`
+    }
+}
+
+class Car extends Vehicle {
+    constructor(make, model, year) {
+        super(make, model, year);
+        this.numWheels = 4;
+    }
+}
+
+class Motorcycle extends Vehicle {
+    constructor(make, model, year) {
+        super(make, model, year);
+        this.numWheels = 2;
+    }
+
+    revEngine() {
+        return "VROOOOOM!"
+    }
+}
+
+class Garage {
+    constructor(capacity) {
+        this.capacity = capacity;
+        this.vehicles = [];
+    }
+
+    add(vehicle) {
+        if (this.vehicles.length >= capcity) {
+            return `Sorry we're full`;
+        }
+
+        if (!(vehicle instanceof Vehicle)) {
+            return `Only vehicles are allowed in here`
+        }
+
+        this.vehicles.push(vehicle);
+        return `Vehicle Added!`
+    }
+}
