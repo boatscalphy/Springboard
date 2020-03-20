decc218558665f803b95bd16dff6b7af275f8b25
Calvin La <calvinhangla.dev@gmail.com>
2020-03-19 18:29:48 -0700

    refactored calc

--- flask-greet-calc/calc/app.py
+++ flask-greet-calc/calc/app.py
@@ -10,16 +10,68 @@ def main():
 
 @app.route('/math/<operation>')
 def math(operation):
-    a = float(request.args['a'])
-    b = float(request.args['b'])
+    
+    try:
+        a = int(request.args['a'])
+        b = int(request.args['b'])
+    except:
+        return 'Invalid inputs!'
 
     if operation == 'add':
-        return f'{a} + {b} = {operations.add(a,b)}'
+        return f'{operations.add(a,b)}'
     elif operation == 'sub':
-        return f'{a} - {b} = {operations.sub(a,b)}'
+        return f'{operations.sub(a,b)}'
     elif operation == 'mult':
-        return f'{a} * {b} = {operations.mult(a,b)}'
+        return f'{operations.mult(a,b)}'
     elif operation == 'div':
-        return f'{a} / {b} = {operations.div(a,b)}'
+        return f'{operations.div(a,b)}'
     else:
         return 'Invalid operation'
+
+@app.route('/add')
+def add():
+    
+    try:
+        a = int(request.args['a'])
+        b = int(request.args['b'])
+    except:
+        return 'Invalid inputs!'
+
+
+    return f'{operations.add(a,b)}'
+
+@app.route('/sub')
+def sub():
+    
+    try:
+        a = int(request.args['a'])
+        b = int(request.args['b'])
+    except:
+        return 'Invalid inputs!'
+
+
+    return f'{operations.sub(a,b)}'
+
+@app.route('/mult')
+def mult():
+    
+    try:
+        a = int(request.args['a'])
+        b = int(request.args['b'])
+    except:
+        return 'Invalid inputs!'
+
+
+    return f'{operations.mult(a,b)}'
+
+@app.route('/div')
+def div():
+    
+    try:
+        a = int(request.args['a'])
+        b = int(request.args['b'])
+    except:
+        return 'Invalid inputs!'
+
+
+    return f'{operations.div(a,b)}'
